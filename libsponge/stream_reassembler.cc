01609f91b8ff472bceec00ac6ab32a90fa27331f
Chen-Carl <987906615@qq.com>
2022-09-17 09:30:01 +0800

    Lab checkpoint 1

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -12,15 +12,70 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity) : 
+    m_output(capacity), 
+    m_capacity(capacity), 
+    m_unassembled(std::map<uint64_t, std::string>()),
+    m_eof(std::numeric_limits<uint64_t>::max()) {
+
+}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    if (eof) {
+        m_eof = index + data.size();
+    }
+    // add to unassembled map
+    size_t maxPos = m_pos + m_capacity - m_output.buffer_size();
+    if (m_pos < maxPos) {
+        size_t len = data.size();
+        if (index + len > maxPos) {
+            len = maxPos - index;
+        }
+        if (m_unassembled.count(index)) {
+            if (m_unassembled[index].size() < len) {
+                m_unassembled[index] = data.substr(0, len);
+            }
+        } else {
+            m_unassembled[index] = data.substr(0, len);
+        }
+    }
+    // check the output stream
+    auto it = m_unassembled.begin();
+    // if it->first <= m_pos, we need to write it to the output stream
+    while (it != m_unassembled.end() && it->first <= m_pos) {
+        if (m_pos - it->first <= it->second.size()) {
+            std::string append = it->second.substr(m_pos - it->first);
+            m_output.write(append);
+            m_pos += append.size();
+            if (m_pos == m_eof) {
+                m_output.end_input();
+            }
+        }
+        m_unassembled.erase(it);
+        it = m_unassembled.begin();
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const { 
+    size_t unassembledBytes = 0;
+    size_t right = 0;
+    for (auto it = m_unassembled.begin(); it != m_unassembled.end(); it++) {
+        size_t begin = it->first;
+        size_t end = begin + it->second.size();
+        if (begin > right) {
+            unassembledBytes += end - begin;
+            right = end;
+        } else if (end > right) {
+            unassembledBytes += end - right;
+            right = end;
+        }
+    }
+    return unassembledBytes;
+}
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { 
+    return m_unassembled.empty(); 
+}
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,6 +5,8 @@
 
 #include <cstdint>
 #include <string>
+#include <map>
+#include <limits>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
@@ -12,8 +14,11 @@ class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
 
-    ByteStream _output;  //!< The reassembled in-order byte stream
-    size_t _capacity;    //!< The maximum number of bytes
+    ByteStream m_output;  //!< The reassembled in-order byte stream
+    size_t m_capacity;    //!< The maximum number of bytes
+    size_t m_pos = 0;    //!< The position of the next byte to be written to the output stream
+    std::map<uint64_t, std::string> m_unassembled;
+    uint64_t m_eof;
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
@@ -33,8 +38,8 @@ class StreamReassembler {
 
     //! \name Access the reassembled byte stream
     //!@{
-    const ByteStream &stream_out() const { return _output; }
-    ByteStream &stream_out() { return _output; }
+    const ByteStream &stream_out() const { return m_output; }
+    ByteStream &stream_out() { return m_output; }
     //!@}
 
     //! The number of bytes in the substrings stored but not yet reassembled
